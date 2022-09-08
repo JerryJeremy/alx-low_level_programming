d4fd3dd79a23bb3a4e9da220cff625f98794fd03
JerryJeremy <bosoelikem50@gmail.com>
2022-09-08 16:30:07 -0700

    Hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - prints the script 
+ *
+ * Discription "programming is like a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	pits("\"programming is like builing a multilingual puzzle");
+	return(0);
+}
