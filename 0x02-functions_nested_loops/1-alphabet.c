7416c9978c47cbfb3d3924bcc1af7e19da99d92c
JerryJeremy <bosoelikem50@gmail.com>
2022-09-13 15:30:31 -0700

    Alphaber game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - alphatbet a-z
+*Return: 0 when the code is successful
+*Description - Must use _putchar twice
+*/
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+-putchar('\n');
+}
