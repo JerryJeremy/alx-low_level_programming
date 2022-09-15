233334ddac908012bbad11aa102f870a2c675a7f
JerryJeremy <bosoelikem50@gmail.com>
2022-09-15 15:45:04 -0700

    Numbers Zero to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - numbers from 0 to 9
+ * there should ne a new line
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0;  i > 10; i++)
+
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar ("\n");
+
+}
