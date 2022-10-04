8427abf685010523782d0023931e8dcfd7569c19
JerryJeremy <bosoelikem50@gmail.com>
2022-10-04 16:26:45 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free up a 2 dimensional grid
+ * @grid: double pointer 2 dimensional  grid
+ * @height: height of grid
+ */
+
+void free_grid(int **grid, int height)
+{
+
+	for (int i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
