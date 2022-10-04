bb7141a5f43af3119f2304c5cb241d67b307dc15
JerryJeremy <bosoelikem50@gmail.com>
2022-10-04 15:55:41 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	x = malloc(size * sizeof(*x));
-	if (a == NULL)
+	if (x == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
